46b4fef6780833a9f7d8dbb32f216c626ccd6df0
Lin <linnegit@gmail.com>
2022-10-16 21:32:07 +0900

    make a route and link to it

---
+++ resources/views/post.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>
+            needs pole soil grabbed pie strike proper outside yesterday early afraid brain doubt pupil tongue on position forgot not birds chain ride chapter next
+        </p>
+    </article>
+    <a href="/">Go Back</a>
+</body>
+
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>
+            needs pole soil grabbed pie strike proper outside yesterday early afraid brain doubt pupil tongue on position forgot not birds chain ride chapter next
+        </p>
+    </article>
+    <article>
+        <h1>My Second Post</h1>
+        <p>
+            needs pole soil grabbed pie strike proper outside yesterday early afraid brain doubt pupil tongue on position forgot not birds chain ride chapter next
+        </p>
+    </article>
+    <article>
+        <h1>My Third Post</h1>
+        <p>
+            needs pole soil grabbed pie strike proper outside yesterday early afraid brain doubt pupil tongue on position forgot not birds chain ride chapter next
+        </p>
+    </article>
+</body>
+
