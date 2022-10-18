41a5e15d2493986e40f570db82a1830fd2463817
Lin <linnegit@gmail.com>
2022-10-18 18:02:20 +0900

    Collection Sorting and Caching ...

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $posts = Post::all();
+
     return view('posts', [
-        'posts' => Post::all()
+        'posts' => $posts
     ]);
 });
 
