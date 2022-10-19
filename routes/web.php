5c907d8054a233e59a70392fee3b9ee781fd11bf
Lin <linnegit@gmail.com>
2022-10-19 18:02:19 +0900

    Clockwork, and the ...

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
@@ -15,7 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $posts = Post::all();
+
+    $posts = Post::with('category')->get();
 
     return view('posts', [
         'posts' => $posts
@@ -26,6 +28,14 @@ Route::get('posts/{post:slug}', function (Post $post) {
 
     return view('post', [
         'post' => $post
+    ]);
+
+});
+
+Route::get('categories/{category:slug}', function (Category $category) {
+
+    return view('posts', [
+        'posts' => $category->posts
     ]);
 
 });
