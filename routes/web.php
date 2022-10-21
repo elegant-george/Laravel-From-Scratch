b4b741c1faeafaa97279bdd77bfaa224d0f8c280
elegant-george <linnegit@gmail.com>
2022-10-21 17:14:55 +0900

    Quick Tweaks and Clean-up

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,10 @@ Route::get('/', function () {
     $posts = Post::latest()->get();
 
     return view('posts', [
-        'posts' => $posts
+        'posts' => $posts,
+        'categories' => Category::all()
     ]);
-});
+})->name('home');
 
 Route::get('posts/{post:slug}', function (Post $post) {
 
@@ -36,15 +37,18 @@ Route::get('posts/{post:slug}', function (Post $post) {
 Route::get('categories/{category:slug}', function (Category $category) {
 
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all()
     ]);
 
-});
+})->name('category');
 
 Route::get('authors/{author:username}', function (User $author) {
 
     return view('posts', [
-        'posts' => $author->posts
+        'posts' => $author->posts,
+        'categories' => Category::all()
     ]);
 
 });
