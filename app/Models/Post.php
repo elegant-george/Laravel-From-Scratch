171e6a708bd240bdf617b253d7015f6195af58a1
elegant-george <linnegit@gmail.com>
2022-10-22 20:35:36 +0900

    Show a Success Flash Message

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -16,12 +16,17 @@ class Post extends Model
     public function scopeFilter($query, array $filters) {
 
         $query->when($filters['search'] ?? false, function ($query, $search) {
-            if ($filters['search'] ?? false) {
-                $query
-                    ->where('title', 'like', '%' . $search . '%')
-                    ->orWhere('body', 'like', '%' . $search . '%');
-            }
+            $query->where(fn($query) =>
+                $query->where('title', 'like', '%' . $search . '%')
+                ->orWhere('body', 'like', '%' . $search . '%'));
+        });
 
+        $query->when($filters['category'] ?? false, function ($query, $category) {
+            $query->whereHas('category', fn($query) => $query->where('slug', $category));
+        });
+
+        $query->when($filters['author'] ?? false, function ($query, $author) {
+            $query->whereHas('author', fn($query) => $query->where('username', $author));
         });
 
     }
