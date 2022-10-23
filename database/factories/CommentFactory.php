35a5a283f9ff9d135f1ca9a5ee69e111f4307132
elegant-george <linnegit@gmail.com>
2022-10-23 15:30:53 +0900

    Design the Comment Form

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -3,6 +3,10 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
@@ -17,7 +21,9 @@ class CommentFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'post_id' => Post::factory(),
+            'user_id' => User::factory(),
+            'body' => $this->faker->paragraph()
         ];
     }
 }
