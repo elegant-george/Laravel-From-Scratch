4854d93be152d1c669f9e5835df290952c79cd78
elegant-george <linnegit@gmail.com>
2022-10-23 21:30:12 +0900

    Make the NewsLetter Form Work

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use App\Models\Category;
@@ -19,17 +20,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('newsletter' , function() {
+    request()->validate(['email' => ['required', 'email']]);
+
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us21'
+    ]);
+
+    try {
+        $response = $mailchimp->lists->addListMember('1b0cee869d', [
+            "email_address" => request('email'),
+            "status" => "subscribed",
+        ]);
+    } catch (\Exception $e) {
+        throw \Illuminate\Validation\ValidationException::withMessages([
+            'email' => 'This email could not be added to our newsletter list.'
+        ]);
+    }
+
+    return redirect('/')->with('success', 'You are now signed up for our newsletter!');
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
-
-Route::get('authors/{author:username}', function (User $author) {
-
-    return view('posts.index', [
-        'posts' => $author->posts,
-    ]);
-
-});
+Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
 
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
